5817c666855dedbbfb770a655305d30fce8b31a2
amin.mokhtari94@gmail.com <amin.mokhtari94@gmail.com>
2023-04-19 13:45:49 +0330

    fix[common]: enum index

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -1,6 +1,5 @@
 export * from './client-type.enum';
 export * from './grant-type.enum';
-export * from './resource.enum';
 export * from './action.enum';
 export * from './scope.enum';
-export * from './geo-json.enum'
+export * from './geo-json.enum';
