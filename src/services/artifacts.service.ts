8b42c43d9115ce636ec61015972cec477a9eaffa
Armin Eskandarinasab <armin3731@live.com>
2023-05-06 15:39:35 +0330

    chore[services]: artifacts

--- src/services/artifacts.service.ts
+++ src/services/artifacts.service.ts
@@ -1,9 +1,22 @@
-import { CreateAxiosDefaults } from 'axios';
+import { AxiosPromise, AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
 import { Artifact } from '../common';
 import { BaseService } from './core';
 
 export class ArtifactsService extends BaseService<Artifact> {
   constructor(protected readonly path: string, protected readonly options?: CreateAxiosDefaults) {
     super(path, options);
+  }
+
+  public async updateMetadataById(
+    id: string,
+    entity: Artifact,
+    config?: AxiosRequestConfig,
+  ): AxiosPromise<Artifact> {
+    return this.patch<Artifact>(`${this.path}/${id}/metadata`, entity, {
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
   }
 }
