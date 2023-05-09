9cce17598038b02334d5cf7908c66ed4caaf2bdc
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-05-09 16:39:01 +0330

    feat[model]: notifications

---
+++ src/common/models/notifier.model.ts
@@ -0,0 +1,12 @@
+import { Resource } from 'common/enums';
+import { BaseModel } from './base.model';
+
+export class NotifierModel extends BaseModel<NotifierModel> {
+  resource!: Resource;
+  receiver!: string;
+  value_args?: string[];
+  template!: string;
+  seen?: boolean;
+  ttl?: number;
+  metadata?: Record<string, unknown>;
+}
