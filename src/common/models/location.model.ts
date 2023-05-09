39b9dc28530824891c8c626f35b5646ea614cda2
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-05-09 16:39:20 +0330

    fix[model]: locations

--- src/common/models/location.model.ts
+++ src/common/models/location.model.ts
@@ -1,7 +1,7 @@
 import { LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon } from 'common/types/geo.type';
 import { BaseModel } from './base.model';
 
-export class LocationModel extends BaseModel<Location> {
+export class LocationModel extends BaseModel<LocationModel> {
   type!: string;
   identity!: string;
   geometry!: LineString | MultiLineString | MultiPoint | MultiPolygon | Point | Polygon;
