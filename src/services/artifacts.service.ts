35b6cd70d0b58453bf7b32623b8f61aad0c4e538
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-05-10 16:33:14 +0330

    fix[service]: overwrite artifactsService

--- src/services/artifacts.service.ts
+++ src/services/artifacts.service.ts
@@ -1,18 +1,170 @@
 import { AxiosPromise, AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
-import { Artifact } from '../common';
-import { BaseService } from './core';
+import {
+  Artifact,
+  ArtifactMetadata,
+  ArtifactModel,
+  Count,
+  Find,
+  UpdateArtifactModel,
+  Create,
+  FindById,
+  UpdateById,
+  DeleteById,
+  RestoreById,
+  UpdateBulk,
+} from '../common';
+import { RequestService } from './core';
+import { AXIOS_CLIENT } from '../common/infrastructure';
 
-export class ArtifactsService extends BaseService<Artifact> {
+export class ArtifactsService extends RequestService<Artifact> {
   constructor(protected readonly path: string, protected readonly options?: CreateAxiosDefaults) {
-    super(path, options);
+    super(AXIOS_CLIENT(options));
+  }
+  public async count({
+    filter,
+    type,
+    config,
+  }: Count<Artifact> & {
+    type: string;
+  }): AxiosPromise<number> {
+    return this.get<number>(`${this.path}/${type}/count`, {
+      params: filter,
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
   }
 
-  public async updateMetadataById(
+  public async create({
+    entity,
+    type,
+    config,
+  }: Create<ArtifactModel> & {
+    type: string;
+  }): AxiosPromise<Artifact> {
+    return this.post<Artifact>(`${this.path}/${type}`, entity, {
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async find({
+    filter,
+    type,
+    config,
+  }: Find<Artifact> & {
+    type: string;
+  }): AxiosPromise<Artifact> {
+    return this.get<Artifact>(`${this.path}/${type}`, {
+      params: filter,
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async findById(
     id: string,
-    entity: Artifact,
-    config?: AxiosRequestConfig,
+    {
+      type,
+      config,
+    }: FindById & {
+      type: string;
+    },
   ): AxiosPromise<Artifact> {
-    return this.patch<Artifact>(`${this.path}/${id}/metadata`, entity, {
+    return this.get<Artifact>(`${this.path}/${type}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async updateById(
+    id: string,
+    {
+      entity,
+      type,
+      config,
+    }: UpdateById<ArtifactModel> & {
+      type: string;
+    },
+  ): AxiosPromise<Artifact> {
+    return this.patch<Artifact>(`${this.path}/${type}/${id}`, entity, {
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async deleteById(
+    id: string,
+    {
+      type,
+      config,
+    }: DeleteById & {
+      type: string;
+    },
+  ): AxiosPromise<Artifact> {
+    return this.delete<Artifact>(`${this.path}/${type}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async restoreById(
+    id: string,
+    {
+      type,
+      config,
+    }: RestoreById & {
+      type: string;
+    },
+  ): AxiosPromise<Artifact> {
+    return this.put<Artifact>(`${this.path}/${type}/${id}/restore`, null, {
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async updateBulk({
+    filter,
+    entity,
+    type,
+    config,
+  }: UpdateBulk<Artifact, UpdateArtifactModel> & {
+    type: string;
+  }): AxiosPromise<number> {
+    return this.patch<number>(`${this.path}/${type}/bulk`, entity, {
+      params: filter,
+      headers: {
+        Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
+      },
+      ...config,
+    });
+  }
+
+  public async updateMetadataByIdentity({
+    identity,
+    metadata,
+    type,
+    config,
+  }: {
+    identity: string;
+    metadata: ArtifactMetadata;
+    type: string;
+    config?: AxiosRequestConfig;
+  }): AxiosPromise<ArtifactMetadata> {
+    return this.patch<ArtifactMetadata>(`${this.path}/${type}/${identity}/metadata`, metadata, {
       headers: {
         Authorization: `Bearer ${(config ?? this.options)?.headers?.common?.Authorization}`,
       },
