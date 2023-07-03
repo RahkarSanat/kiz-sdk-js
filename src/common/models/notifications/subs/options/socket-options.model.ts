cda03df043b249f8d59690fa901c39a7a4d088bc
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-07-03 11:21:00 +0330

    fix[model]: socket level option type

--- src/common/models/notifications/subs/options/socket-options.model.ts
+++ src/common/models/notifications/subs/options/socket-options.model.ts
@@ -1,8 +1,9 @@
+import { SocketLevel } from '../../../../enums';
 import { Options } from './base-options.model';
 
 export class SocketOptions extends Options {
   channels!: string[];
   links?: [];
-  level?: string;
+  level?: SocketLevel;
   indicator?: string;
 }
