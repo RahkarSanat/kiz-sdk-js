e88ee375a3916efdc907471d4b3b98ccd716350c
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-07-05 12:00:10 +0330

    chore: order

--- src/common/interfaces/logistics/vehicle.interface.ts
+++ src/common/interfaces/logistics/vehicle.interface.ts
@@ -2,10 +2,10 @@ import { Base } from '../base';
 import { Violation } from './violation.interface';
 
 export interface Vehicle<Meta = Record<string, unknown>> extends Base<Meta> {
-  drivers?: string[];
-  model?: string;
-  plate?: string[];
   type?: string;
+  plate?: string[];
+  model?: string;
   violations?: Violation[];
   year?: number;
+  drivers?: string[];
 }
