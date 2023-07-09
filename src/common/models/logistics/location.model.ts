abe600540e00f195da8be924ce9bb5925697f3ac
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-07-09 18:07:48 +0330

    fix: optional location metadata

--- src/common/models/logistics/location.model.ts
+++ src/common/models/logistics/location.model.ts
@@ -1,7 +1,7 @@
 import { Geometry } from '../../types';
 import { BaseModel } from '../base.model';
 
-export class LocationModel<Meta, Prop = Record<string, unknown>> extends BaseModel<
+export class LocationModel<Meta = Record<string, unknown>, Prop = Record<string, unknown>> extends BaseModel<
   LocationModel<Meta, Prop>,
   Meta
 > {
