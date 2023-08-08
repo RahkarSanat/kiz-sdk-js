da67e815d0c538d5f1805e5c54a613ec610fb60f
Armin Eskandarinasab <armin3731@live.com>
2023-08-08 13:36:35 +0330

    fix[workflow]: Status

--- src/common/enums/workflow-status.enum.ts
+++
@@ -1,8 +0,0 @@
-export declare enum WorkflowStatus {
-  Ready = 'ready',
-  Paused = 'paused',
-  Failed = 'failed',
-  Running = 'running',
-  Completed = 'completed',
-  Terminated = 'terminated',
-}
--- src/common/models/workflow.model.ts
+++ src/common/models/workflow.model.ts
@@ -1,10 +1,9 @@
 import { WorkflowToken } from '../interfaces';
-import { WorkflowStatus } from '../enums';
 import { BaseModel } from './base.model';
 
 export class WorkflowModel extends BaseModel<WorkflowModel> {
   type!: string;
   data?: unknown;
-  status!: WorkflowStatus;
+  status!: string;
   tokens!: WorkflowToken[];
 }
