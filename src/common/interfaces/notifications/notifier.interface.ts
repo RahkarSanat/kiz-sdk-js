439bee22d05f00200fb5a448f8ada9619e6f4330
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-08-12 17:08:18 +0330

    fix[type]: notifier

--- src/common/interfaces/notifications/notifier.interface.ts
+++ src/common/interfaces/notifications/notifier.interface.ts
@@ -1,12 +1,56 @@
-import { Resource } from '../../enums';
 import { Base } from '../base';
 
-//TODO: must be refactor after finishing tests.
-export interface Notifier<Meta = Record<string, unknown>> extends Base<Meta> {
-  resource: Resource;
+export interface KavenegarEntry {
+  messageid: number;
+
+  message: string;
+
+  status: number;
+
+  statustext: string;
+
+  sender: string;
+
   receptor: string;
-  template: string;
-  value_args?: string[];
-  seen?: Record<string, boolean>;
-  status?: Record<string, boolean>;
+
+  date: number;
+
+  cost: number;
 }
+
+export interface KavenegarResponse {
+  entries: KavenegarEntry[];
+  message: string;
+  status: number;
+}
+
+export interface NotifierStatistics {
+  sms?: {
+    status: Record<string, KavenegarResponse>[]; // or any other supported provider response type
+    message: string;
+  };
+  mail?: {
+    status: Record<string, KavenegarResponse>[];
+    message: string;
+  };
+  fcm?: {
+    status: Record<string, KavenegarResponse>[];
+    message: string;
+  };
+  socket?: {
+    status: Record<string, KavenegarResponse>[];
+    message: string;
+  };
+}
+
+export interface Notifier<Meta = Record<string, unknown>> extends Base<Meta> {
+  reason: string;
+  triggers: string;
+  value_args?: string[];
+  statistic?: NotifierStatistics;
+}
+
+export type NotificationNotifierInterface = Notifier;
+export type NotificationNotifierStatisticsInterface = NotifierStatistics;
+export type NotificationNotifierKavenegarResponseInterface = KavenegarResponse;
+export type NotificationNotifierKavenegarEntryInterface = KavenegarEntry;
