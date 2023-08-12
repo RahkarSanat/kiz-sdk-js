5c2dc98cbc480fa7d04e192e12ec25435fd7c6c7
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-08-12 17:10:34 +0330

    fix[type]: recipients

---
+++ src/common/models/notifications/subs/recipient-to.model.ts
@@ -0,0 +1,9 @@
+export class ToModel {
+  alias?: string;
+  phone_number?: string;
+  subject?: string;
+  user_id?: string;
+  active_items?: string[];
+}
+
+export type NotificationRecipientToModel = ToModel;
