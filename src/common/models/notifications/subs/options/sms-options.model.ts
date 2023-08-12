c0ffca4dc874e873ac2255c59acb17240feeee66
mmRoshani <mohammadmojtabaroshani@outlook.com>
2023-08-12 17:10:52 +0330

    fix[type]: templates

--- src/common/models/notifications/subs/options/sms-options.model.ts
+++ src/common/models/notifications/subs/options/sms-options.model.ts
@@ -3,5 +3,6 @@ import { Options } from './base-options.model';
 
 export class SmsOptions extends Options {
   functionality!: SmsFunctionality;
+  provider_lookup_template_name?: string;
   retry?: boolean;
 }
